96c1967590653a32c76fa4f00d8836aa203663a8
revant.sinha@gmail.com <revant.sinha@gmail.com>
2026-03-23 09:44:11 +0530

    another useless commit --life is useless-- ignore

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 int main() {
     PreMatchAttackComputation::init();
     Board startPos;
-    tests::runPerftSuite(startPos, STARTING_POSITION_PERFT, "Starting Position");
+    // tests::runPerftSuite(startPos, STARTING_POSITION_PERFT, "Starting Position");
     Board kiwiBoard(KIWIPETE_PERFT_START_FEN);
-    tests::runPerftSuite(kiwiBoard, KIWIPETE_PERFT, "KIWIPETE Position");
+    // tests::runPerftSuite(kiwiBoard, KIWIPETE_PERFT, "KIWIPETE Position");
+    // tests::perftDivide(kiwiBoard, 2);
 }
