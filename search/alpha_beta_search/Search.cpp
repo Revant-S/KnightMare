454719386317c925ab5e6ea93aa0a875dbe81da0
Satrajeet Paul <paulsatrajeet@Satrajeets-MacBook-Pro.local>
2026-05-18 23:19:28 +0530

    Alpha-Beta pruning implemented in MinMax

--- search/alpha_beta_search/Search.cpp
+++ search/alpha_beta_search/Search.cpp
@@ -17,13 +17,13 @@ namespace Search {
     const int INF = 1000000;
     const int MATE_SCORE = -100000;
 
-    int minMax(const int depth, Board &board) {
+    int minMax(const int depth, int alpha, int beta, Board &board) { //alpha, beta parameters added
         // Base case: static evaluation
         if (depth == 0) {
             return Evaluation::evaluate(board);
         }
 
-        int maxScore = -INF;
+        //int maxScore = -INF; removed this, alpha tracks
         MoveList legalMoves = MoveFunctions::getAllLegalMoves(board);
 
         // Checkmate and Stalemate =
@@ -38,11 +38,14 @@ namespace Search {
         for (Move &move: legalMoves) {
             BoardState savedState = board.saveState();
             board.makeMove(move);
-            int score = -minMax(depth - 1, board);
+            int score = -minMax(depth - 1, -beta, -alpha, board); //- sign, because we basically reverse the limits
+            //when the model predicts future opponent moves(imp: Swap alpha and beta position)
             board.unmakeMove(savedState);
-            maxScore = std::max(maxScore, score);
+            //maxScore = std::max(maxScore, score);
+            if (score >= beta) return beta;
+            if (score > alpha) alpha =  score;
         }
-        return maxScore;
+        return alpha;
     }
 
     Move getBestMove(Board &board) {
@@ -51,8 +54,10 @@ namespace Search {
             return {};
         }
 
-        int maxScore = -INF;
+        //int maxScore = -INF;
         Move bestMove = legalMoves[0];
+        int alpha = -INF;
+        int beta = INF;
 
         // Ensure DEPTH_OF_SEARCH is at least 2, otherwise it can't see mates!
         int currentDepth = std::max(2, DEPTH_OF_SEARCH);
@@ -60,12 +65,12 @@ namespace Search {
             BoardState savedState = board.saveState();
             board.makeMove(move);
             // board.toggle_side(); // REMOVE THIS IF makeMove() ALREADY TOGGLES THE TURN
-            int score = -minMax(currentDepth - 1, board);
+            int score = -minMax(currentDepth - 1, -beta, -alpha, board);
             // EvaluationUtils::printScoreBreakDown(board, move);
             board.unmakeMove(savedState);
             // std::cout << "Move : " << Utils::moveToString(move) << " has calculate score of <<" << score << "\n";
-            if (score > maxScore) {
-                maxScore = score;
+            if (score > alpha) {
+                alpha = score;
                 bestMove = move;
             }
         }
@@ -73,5 +78,6 @@ namespace Search {
     }
 
     MoveList OrderMoves(MoveList &moveList) {
+        return moveList; //need to change, just to pass compilation, returning same
     }
 } // Search
